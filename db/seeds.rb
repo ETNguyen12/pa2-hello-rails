da7d68be39c2e1c02aa6cefe90140a6bcde016ce
Ethan Nguyen <nguyeeth@tamu.edu>
2024-12-31 03:09:39 +0000

    Add more movies

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,18 +9,16 @@
 #   end
 # Seed the RottenPotatoes DB with some movies.
 more_movies = [
-  {:title => 'My Neighbor Totoro', :rating => 'G',
-    :release_date => '16-Apr-1988'},
-  {:title => 'Green Book', :rating => 'PG-13',
-    :release_date => '16-Nov-2018'},
-  {:title => 'Parasite', :rating => 'R',
-    :release_date => '30-May-2019'},
-  {:title => 'Nomadland', :rating => 'R',
-    :release_date => '19-Feb-2021'},
-  {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+  { title: 'My Neighbor Totoro', rating: 'G', release_date: '16-Apr-1988' },
+  { title: 'Green Book', rating: 'PG-13', release_date: '16-Nov-2018' },
+  { title: 'Parasite', rating: 'R', release_date: '30-May-2019' },
+  { title: 'Nomadland', rating: 'R', release_date: '19-Feb-2021' },
+  { title: 'CODA', rating: 'PG-13', release_date: '13-Aug-2021' },
+  { title: 'The Grand Budapest Hotel', rating: 'R', release_date: '28-Mar-2014' },
+  { title: 'Whiplash', rating: 'R', release_date: '15-Oct-2014' },
+  { title: 'The Social Network', rating: 'PG-13', release_date: '1-Oct-2010' }
 ]
 
 more_movies.each do |movie|
-  Movie.create!(movie)
+  Movie.find_or_create_by(title: movie[:title], rating: movie[:rating], release_date: movie[:release_date])
 end
