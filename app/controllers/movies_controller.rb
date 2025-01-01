fad772df9e5ea2bfdfbe3130811bba4ec29cb699
Ethan Nguyen <nguyeeth@tamu.edu>
2025-01-01 14:12:38 +0000

    Fixing sorting

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,42 +3,47 @@ class MoviesController < ApplicationController
 
   # GET /movies or /movies.json
   def index
-    if params[:sort].present? && params[:direction].present?
-      session[:sort] = params[:sort]
-      session[:direction] = params[:direction]
-    end
+    @sort = params[:sort]
+    @direction = params[:direction]
   
-    sort_column = session[:sort]
-    sort_direction = session[:direction]
+    subquery = Movie.select('MIN(id) as id').group(:title, :release_date)
+    @movies = Movie.where(id: subquery)
   
-    if sort_column.nil? || sort_direction == 'none'
-      @movies = Movie.all
-    else
-      sort_direction = %w[asc desc].include?(sort_direction) ? sort_direction : 'asc'
-      @movies = Movie.order(sort_column => sort_direction)
+    if @sort.present? && @direction.present?
+      case @sort
+      when 'release_date'
+        @movies = @movies.order(Arel.sql("release_date #{@direction}"))
+      else
+        @movies = @movies.order(Arel.sql("LOWER(#{ActiveRecord::Base.connection.quote_column_name(@sort)}) #{@direction}"))
+      end
     end
-  end  
-
+  end
+  
   # GET /movies/1 or /movies/1.json
   def show
-  end
+    @sort = params[:sort]
+    @direction = params[:direction]
+  end  
 
   # GET /movies/new
   def new
     @movie = Movie.new
+    @sort = params[:sort]
+    @direction = params[:direction]
   end
 
   # GET /movies/1/edit
   def edit
+    @sort = params[:sort]
+    @direction = params[:direction]
   end
 
   # POST /movies or /movies.json
   def create
     @movie = Movie.new(movie_params)
-
     respond_to do |format|
       if @movie.save
-        format.html { redirect_to @movie, notice: "Movie was successfully created." }
+        format.html { redirect_to movies_path(sort: params[:sort], direction: params[:direction]), notice: "Movie was successfully created." }
         format.json { render :show, status: :created, location: @movie }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -51,7 +56,7 @@ class MoviesController < ApplicationController
   def update
     respond_to do |format|
       if @movie.update(movie_params)
-        format.html { redirect_to @movie, notice: "Movie was successfully updated." }
+        format.html { redirect_to movie_path(@movie, sort: params[:sort], direction: params[:direction]), notice: "Movie was successfully updated." }
         format.json { render :show, status: :ok, location: @movie }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -63,20 +68,20 @@ class MoviesController < ApplicationController
   # DELETE /movies/1 or /movies/1.json
   def destroy
     @movie.destroy!
-
     respond_to do |format|
-      format.html { redirect_to movies_path, status: :see_other, notice: "Movie was successfully destroyed." }
+      format.html { redirect_to movies_path(sort: params[:sort], direction: params[:direction]), status: :see_other, notice: "Movie was successfully destroyed." }
       format.json { head :no_content }
     end
   end
 
   private
-
+    # Use callbacks to share common setup or constraints between actions.
     def set_movie
-      @movie = Movie.find(params.require(:id))
+      @movie = Movie.find(params.expect(:id))
     end
 
+    # Only allow a list of trusted parameters through.
     def movie_params
-      params.require(:movie).permit(:title, :rating, :description, :release_date)
+      params.expect(movie: [ :title, :rating, :description, :release_date ])
     end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,24 @@
 module ApplicationHelper
-end
+    # Determine the next sorting direction (asc -> desc -> nil)
+    def next_direction(column)
+      if @sort == column
+        case @direction
+        when 'asc'
+          'desc'
+        when 'desc'
+          nil # No sorting
+        else
+          'asc'
+        end
+      else
+        'asc' # Default to ascending if sorting a new column
+      end
+    end
+  
+    # Display the current sort indicator (⬆, ⬇, or blank)
+    def sort_indicator(column)
+      return '⬆' if @sort == column && @direction == 'asc'
+      return '⬇' if @sort == column && @direction == 'desc'
+      '' # No indicator if no sorting
+    end
+  end  
